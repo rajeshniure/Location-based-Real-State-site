f37ca4a999c81d2d84afb2b025487df1ac9ebf70
Rajesh Niure <rajeshniure567@gmail.com>
2025-12-16 01:58:43 +0545

    adding image fields and rendering the images in react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = 'users.User'
--- backend/listings/forms.py
+++ backend/listings/forms.py
@@ -5,7 +5,7 @@ from django.contrib.gis.geos import Point
 class ListingsForm(forms.ModelForm):
     class Meta:
         model = Listing
-        fields = ['title','description','area','borough','listing_type','property_status','price','rental_frequency','rooms','furnished','pool','elevator','cctv','parking','date_posted','location','latitude','longitude']
+        fields = ['title','description','area','borough','listing_type','property_status','price','rental_frequency','rooms','furnished','pool','elevator','cctv','parking','date_posted','location','latitude','longitude','picture1','picture2','picture3','picture4','picture5']
         
         
     latitude = forms.FloatField()
